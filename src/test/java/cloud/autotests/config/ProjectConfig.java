2f4b1cb127a306f0d099e806af263de11cd3ade9
aKuslin123 <a.Kuslin123@gmail.com>
2021-11-30 13:06:03 +0700

    added local.properties.example

--- src/test/java/cloud/autotests/config/ProjectConfig.java
+++ src/test/java/cloud/autotests/config/ProjectConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/local.properties",
+        "classpath:config/local.properties.example",
         "classpath:config/remote.properties"
 })
 public interface ProjectConfig extends Config {
